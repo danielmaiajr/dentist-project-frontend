d16a9c5b6da8e3973586afe98f2749a6ab7de942
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-09 21:27:02 -0300

    Create redux store, reducers and typescript helper functions for dispatch and selector

---
+++ src/context/AuthSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export interface AuthState {
+  validToken: boolean;
+  token: string;
+}
+
+const initialState: AuthState = {
+  validToken: false,
+  token: "",
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<string>) => {
+      state.validToken = true;
+      state.token = action.payload;
+    },
+
+    logout: (state) => {
+      state.validToken = false;
+      state.token = "";
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
