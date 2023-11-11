5e37d25ec70cd545daafcf9b0b7cd598f342a1e2
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-11 20:31:11 -0300

    Added login and register buttons on the pages

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import auth from "../auth/axios";
 
@@ -7,12 +7,12 @@ import { login } from "../context/AuthSlice";
 import { useAppDispatch } from "../hooks";
 
 import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Button, buttonVariants } from "@/components/ui/button";
+
+import { cn } from "@/lib/utils";
 
 const Login = () => {
-  const navigate = useNavigate();
-
   const dispatch = useAppDispatch();
 
   const [email, setEmail] = useState<string>("");
@@ -23,68 +23,75 @@ const Login = () => {
     const response = await auth.login(email, password);
 
     dispatch(login(response?.data?.token));
-    navigate("/");
   };
 
   return (
-    <div className="container relative h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:px-0">
-      <div className="lg:p-8 mx-auto flex h-full">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Entre na sua conta
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Coloque seu email abaixo para entrar
-            </p>
-          </div>
-          <div className="grid gap-5">
-            <form onSubmit={handleSubmit}>
-              <div className="grid gap-3">
-                <div className="grid gap-3">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="nome@exemplo.com.br"
-                    autoCapitalize="none"
-                    autoComplete="email"
-                    autoCorrect="off"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                  />
+    <div className="container relative h-full flex-col items-center justify-center md:grid lg:max-w-none lg:px-0">
+      <Link
+        to={"/register"}
+        className={cn(
+          buttonVariants({ variant: "outline" }),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        Cadastre-se
+      </Link>
 
-                  <Label htmlFor="password">Senha</Label>
-                  <Input
-                    type="password"
-                    id="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                  />
-                </div>
-                <Button>Entrar</Button>
-              </div>
-            </form>
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
-              </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
-                  ou continue com
-                </span>
-              </div>
-            </div>
-            <Button variant="outline" type="button">
-              Google
-            </Button>
-          </div>
-          <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our Terms of Service and Privacy
-            Policy.
+      <div className="pb-12 mx-auto flex h-full w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Entre na sua conta
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Coloque seu email abaixo para entrar
           </p>
         </div>
+        <div className="grid gap-5">
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-3">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="nome@exemplo.com.br"
+                  autoCapitalize="none"
+                  autoComplete="email"
+                  autoCorrect="off"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+
+                <Label htmlFor="password">Senha</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required
+                />
+              </div>
+              <Button>Entrar</Button>
+            </div>
+          </form>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                ou continue com
+              </span>
+            </div>
+          </div>
+          <Button variant="outline" type="button" disabled>
+            Google
+          </Button>
+        </div>
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          By clicking continue, you agree to our Terms of Service and Privacy
+          Policy.
+        </p>
       </div>
     </div>
   );
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,38 +1,106 @@
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import auth from "../auth/axios";
 
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
 const Register = () => {
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [confirmPwd, setConfirmPwd] = useState<string>("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    auth.signup(email, password);
+    if (password === confirmPwd) {
+      auth.signup(email, password);
+      navigate("/login");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username:</label>
-      <input
-        type="text"
-        id="username"
-        autoComplete="off"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        required
-      />
+    <div className="container relative h-full flex-col items-center justify-center md:grid lg:max-w-none lg:px-0">
+      <Link
+        to={"/login"}
+        className={cn(
+          buttonVariants({ variant: "outline" }),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        Login
+      </Link>
+      <div className="pb-12 mx-auto flex h-full w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Crie sua conta
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Coloque seu email abaixo para criar conta
+          </p>
+        </div>
 
-      <label htmlFor="password">Password:</label>
-      <input
-        type="password"
-        id="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-        required
-      />
-      <button>Sign In</button>
-    </form>
+        <div className="grid gap-5">
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-3">
+              <div className="grid gap-3">
+                <Label htmlFor="username">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="nome@exemplo.com.br"
+                  autoCapitalize="none"
+                  autoComplete="email"
+                  autoCorrect="off"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+
+                <Label htmlFor="password">Senha</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required
+                />
+
+                <Label htmlFor="password">Confirmar senha</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  onChange={(e) => setConfirmPwd(e.target.value)}
+                  value={confirmPwd}
+                  required
+                />
+              </div>
+              <Button>Criar</Button>
+            </div>
+          </form>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                ou continue com
+              </span>
+            </div>
+          </div>
+          <Button variant="outline" type="button" disabled>
+            Google
+          </Button>
+        </div>
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          By clicking continue, you agree to our Terms of Service and Privacy
+          Policy.
+        </p>
+      </div>
+    </div>
   );
 };
 
