48c8aefd6aa8fd42f12ef27fa0cab040cfc055ab
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-12 17:40:06 -0300

    Added UserSlice to the redux store

--- src/context/store.ts
+++ src/context/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authSlice from "./AuthSlice";
+import userSlice from "./UserSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
+    user: userSlice,
   },
 });
 
