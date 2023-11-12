cb91a72e538c97311febb1299ef39aaba9b069ee
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-12 17:49:33 -0300

    Added patientSlice to the redux store

--- src/context/store.ts
+++ src/context/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authSlice from "./AuthSlice";
 import userSlice from "./UserSlice";
+import patientSlice from "./PatientSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     user: userSlice,
+    patient: patientSlice,
   },
 });
 
