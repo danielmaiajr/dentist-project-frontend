721e391fb4671e089269a6f6bb0137fc1e7af78d
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-12 10:12:49 -0300

    Added basic layout to PatientPage

--- src/components/PatientPage.tsx
+++ src/components/PatientPage.tsx
@@ -1,3 +1,8 @@
-const PatientPage = () => <div>Pacientes</div>;
+const PatientPage = () => (
+  <div className="w-full px-10 py-5">
+    <h1 className="text-2xl font-semibold tracking-tight pb-1">Pacientes</h1>
+    <p className="text-sm text-muted-foreground">Lista de Pacientes</p>
+  </div>
+);
 
 export default PatientPage;
