d61fc513016d0a23779224c51c660869ba36d9e5
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-13 15:34:16 -0300

    List patients on the page for test

--- src/components/PatientPage.tsx
+++ src/components/PatientPage.tsx
@@ -1,8 +1,26 @@
-const PatientPage = () => (
-  <div className="w-full px-10 py-5">
-    <h1 className="text-2xl font-semibold tracking-tight pb-1">Pacientes</h1>
-    <p className="text-sm text-muted-foreground">Lista de Pacientes</p>
-  </div>
-);
+import { getAllPatients } from "@/context/PatientSlice";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { useEffect } from "react";
+
+const PatientPage = () => {
+  const dispatch = useAppDispatch();
+  const patients = useAppSelector((state) => state.patient.patients);
+
+  useEffect(() => {
+    dispatch(getAllPatients());
+  }, []);
+
+  return (
+    <div className="w-full px-10 py-5">
+      <h1 className="text-2xl font-semibold tracking-tight pb-1">Pacientes</h1>
+      <p className="text-sm text-muted-foreground">Lista de Pacientes</p>
+      {patients.map((p) => (
+        <div key={JSON.stringify(p.id)} className="text-sm">
+          {JSON.stringify(p)}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default PatientPage;
