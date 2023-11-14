3114a07b5914de9d32ed6baa7421ceeaa75601a2
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-14 20:27:40 -0300

    Added basic insurance list to insurancePage

--- src/components/InsurancePage.tsx
+++ src/components/InsurancePage.tsx
@@ -1,5 +1,19 @@
+import { useAppSelector } from "@/hooks";
+
 const InsurancePage = () => {
-  return <div>Planos</div>;
+  const insurances = useAppSelector((state) => state.insurances);
+
+  return (
+    <div className="w-full px-10 py-5">
+      <h1 className="text-2xl font-semibold tracking-tight pb-1">Planos</h1>
+      <p className="text-sm text-muted-foreground">Lista de Planos</p>
+      {insurances.map((i) => (
+        <div key={JSON.stringify(i.id)} className="text-sm">
+          {JSON.stringify(i)}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default InsurancePage;
