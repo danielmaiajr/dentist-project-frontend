81d38d6405f62a979df4bdfaf548661b61518223
Daniel Maia <daniel.maia.jr12@gmail.com>
2023-11-16 08:44:42 -0300

    Added basic appointment list to SchedulerPage

--- src/components/SchedulerPage.tsx
+++ src/components/SchedulerPage.tsx
@@ -1,3 +1,19 @@
-const schedulerPage = () => <div>Agenda</div>;
+import { useAppSelector } from "@/hooks";
+
+const schedulerPage = () => {
+  const appointments = useAppSelector((state) => state.appointments);
+
+  return (
+    <div className="w-full px-10 py-5">
+      <h1 className="text-2xl font-semibold tracking-tight pb-1">Consultas</h1>
+      <p className="text-sm text-muted-foreground">Lista de consultas</p>
+      {appointments.map((a) => (
+        <div key={JSON.stringify(a.id)} className="text-sm">
+          {JSON.stringify(a)}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default schedulerPage;
